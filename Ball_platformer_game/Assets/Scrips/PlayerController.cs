ca80a4e184910ba8a638087e9aa97d4ad7973d82
Yifei Jin <yj819@ic.ac.uk>
2021-01-20 19:49:18 +0800

    Ball_platformer_game_v1
    From Yifei

---
+++ Ball_platformer_game/Assets/Scrips/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(0.0f, 0.0f, 1.0f);
+        rb.AddForce(movement*force);
+        //apply force to keep the ball moving
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey("a")) {Debug.Log("left"); }
+        if (Input.GetKey("s")) { Debug.Log("middle"); }
+        if (Input.GetKey("d")) { Debug.Log("right"); }
+    }
+}
