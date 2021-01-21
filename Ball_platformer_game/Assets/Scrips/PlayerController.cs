a9cc453fd420816922e6314bb1c241424778d784
Yifei Jin <yj819@ic.ac.uk>
2021-01-21 15:57:36 +0800

    3 Lanes
    Keyboard:
    'a' represent left lane
    's' represent middle lane
    'd' represent right lane

--- Ball_platformer_game/Assets/Scrips/PlayerController.cs
+++ Ball_platformer_game/Assets/Scrips/PlayerController.cs
@@ -7,24 +7,48 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     public float force;
 
+    public Transform start;
+    public Transform left;
+    public Transform right;
+    public Transform middle;
+    public float speed;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();        
+        rb = GetComponent<Rigidbody>();
+
     }
 
-    // Update is called once per frame
+    //Apply force to make the ball contineously moving, speed can be changed by the public vatriable speed
     void FixedUpdate()
     {
-        Vector3 movement = new Vector3(0.0f, 0.0f, 1.0f);
-        rb.AddForce(movement*force);
+        Vector3 movement = Vector3.forward;
+        rb.AddForce(movement * force);
+
         //apply force to keep the ball moving
     }
 
+
+    //when input"a" the ball will move to the Left lanes, input"s" will move to the middle lanes, input "d"will move to right lanes
+    //When we use kinect can just change the input, relate it to 3 movement
     private void Update()
     {
-        if (Input.GetKey("a")) {Debug.Log("left"); }
-        if (Input.GetKey("s")) { Debug.Log("middle"); }
-        if (Input.GetKey("d")) { Debug.Log("right"); }
+        if (Input.GetKey("a"))
+        {
+            transform.position = Vector3.MoveTowards(start.position, left.position, speed * Time.deltaTime);
+            //Debug.Log("left"); 
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position = Vector3.MoveTowards(start.position, middle.position, speed * Time.deltaTime);
+            //Debug.Log("middle"); 
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position = Vector3.MoveTowards(start.position, right.position, speed * Time.deltaTime);
+            //Debug.Log("right");
+        }
     }
 }
