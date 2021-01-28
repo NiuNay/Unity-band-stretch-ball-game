fbd40ad7b386cec9cec941e331b190982b7f21ff
Yifei Jin <yj819@ic.ac.uk>
2021-01-28 12:55:46 +0800

    Ball_Platformer_Game_v3
    Add Pause funtion, can stop then restart

---
+++ Ball_platformer_game/Assets/Scrips/Pause_Control.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause_Control : MonoBehaviour
+{
+    public GameObject PauseMenu;
+
+    private void Start()
+    {
+        PauseMenu.SetActive(false);
+    }
+
+    public void OnPause()
+    {
+        Time.timeScale = 0;
+        PauseMenu.SetActive(true);
+    }
+
+    public void OnResume()
+    {
+        Time.timeScale = 1f;
+        PauseMenu.SetActive(false);
+    }
+
+    public void OnRestart()
+    {
+        //UnityEngin.SceneManagement.SceneManager.LoadScene();
+        Time.timeScale = 1f;
+    }
+}
