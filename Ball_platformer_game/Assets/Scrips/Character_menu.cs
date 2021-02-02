1df71775277e54330c65164c9af6b4d80c394aa2
NiuNay <agp119@ic.ac.uk>
2021-02-02 13:43:18 +0000

    Character menu
    Made character menu (but the character still doesn't get modified in the actual game page).

---
+++ Ball_platformer_game/Assets/Scrips/Character_menu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Character_menu : MonoBehaviour
+{
+    Sprite[] spritesheet;
+    int colourID;
+    Image representation;
+
+    //Use this for initialization
+    void Start()
+    {
+        representation = GameObject.Find("Representation").GetComponent<Image>();
+        colourID = 0;
+        spritesheet = Resources.LoadAll<Sprite>("ball_sprites");
+    }
+
+    public void ChangeColour()
+    {
+        if (colourID < 2)
+        {
+            colourID++;
+            Debug.Log(colourID);
+        }
+        else if (colourID == 2)
+        {
+            colourID = 0;
+        }
+
+        foreach (Sprite S in spritesheet)
+        {
+            if (S.name.Equals("ball_sprites_" + colourID))
+            {
+                representation.sprite = S;
+            }
+        }
+    }
+}
